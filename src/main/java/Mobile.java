52ed734a920da5e69c89d344a5dfbec66bf1d288
KEN MICHAEL SEVILLA (SHP-IRIS-ISD-OOCLL/MNL) <SEVILKE@OOCL.COM>
2019-10-10 13:38:47 +0800

    story 2

--- src/main/java/Mobile.java
+++ src/main/java/Mobile.java
@@ -3,7 +3,35 @@ public class Mobile {
     private String color;
     private String brand;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
     public void call(String message){
         System.out.println("Message: " + message);
+    }
+
+    public void getInformation(){
+        System.out.println("name: " + this.name + ", color: " + this.color + ", brand: " + this.brand);
     }
 }
